c1905c489ee6b9a373ff3f116edc4a3b0cb26c68
ArikB08 <arikbagchi@outlook.com>
2025-11-17 15:21:20 +0800

    sfasdv

--- SparkleTotsTeeth/LowTierView.swift
+++ SparkleTotsTeeth/LowTierView.swift
@@ -4,43 +4,6 @@
 //
 //  Created by Arik Bagchi on 17/11/25.
 //
-//
-//import SwiftUI
-//
-//struct LowTierView: View {
-//    @State private var showingDoneAlert = false
-//    
-//    var body: some View {
-//        VStack{
-//            Spacer()
-//            
-//            Button("                             DONE                               ", systemImage: "checkmark.circle"){
-//                showingDoneAlert = true
-//            }
-//            .buttonStyle(.borderedProminent)
-//            .alert("Are you sure?", isPresented: $showingDoneAlert) {
-//                Button("Cancel", role: .cancel) {
-//                    // notif disappears
-//                }
-//                Button("Submit", role: .none) {
-//                    // submitted to database
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//#Preview {
-//    LowTierView()
-//}
-
-
-//
-//  UncertainView.swift
-//  SparkleTotsTeeth
-//
-//  Created by Arik Bagchi on 17/11/25.
-//
 
 import SwiftUI
 
